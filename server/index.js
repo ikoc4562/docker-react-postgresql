65294a82ee2046f916b580e7aeeaca6c4fbbb106
ISA KOC <ikoc4562@gmail.com>
2024-05-06 22:43:25 +0300

    client added

--- server/index.js
+++ server/index.js
@@ -53,6 +53,52 @@ app.get('/data', async (req, res) => {
   }
 });
 
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await pool.query(
+      'SELECT * FROM users WHERE email = $1 AND name = $2',
+      [email, password]
+    );
+
+    if (result.rows.length > 0) {
+      // Kullanıcı bulunduğunda başarılı yanıt
+      res.json({ message: 'Oturum açıldı', user: result.rows[0] });
+    } else {
+      // Kullanıcı bulunamadığında hata yanıtı
+      res.status(401).json({ error: 'E-posta veya isim hatalı' });
+    }
+  } catch (error) {
+    console.error('Database error:', error);
+    res.status(500).json({ error: 'Sunucu hatası' });
+  }
+});
+
+
+// İsme göre kullanıcı verilerini almak için GET rotası
+app.get('/users/email/:email', async (req, res) => {
+  const { email } = req.params; // İsim parametresini al
+  try {
+    const result = await pool.query(
+      'SELECT * FROM users WHERE email = $1',
+      [email]
+    );
+
+    if (result.rows.length > 0) {
+      // Eğer kullanıcı bulunduysa, bilgileri döndür
+      res.json(result.rows);
+    } else {
+      // Eğer isimle eşleşen kullanıcı yoksa, hata mesajı gönder
+      res.status(404).json({ error: 'Kullanıcı bulunamadı' });
+    }
+  } catch (error) {
+    console.error('Veritabanı hatası:', error);
+    res.status(500).send('Sunucu hatası');
+  }
+});
+
+
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Express.js sunucusu port ${PORT} üzerinde çalışıyor`);
